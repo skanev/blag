b65c88d54fb5e28c15b77e73871a76714595e6fc
Stefan Kanev <stefan.kanev@gmail.com>
2010-05-15 08:36:39 +0200

    Given a better name to a route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 ActionController::Routing::Routes.draw do |map|
   map.resources :posts
-  map.connect ":year/:month/:day/:slug", :controller => :word_press_links, :action => :date_and_slug, :year => /\d{4}/, :month => /\d{2}/, :day => /\d{2}/
+
+  map.word_press_date_and_slug ":year/:month/:day/:slug",
+                               :controller => :word_press_links,
+                               :action => :date_and_slug,
+                               :year => /\d{4}/,
+                               :month => /\d{2}/,
+                               :day => /\d{2}/
   map.word_press_old_id '/word_press/old/:old_id', :controller => :word_press_links, :action => :old_id
+
   map.root :controller => :home, :action => :index
 end
