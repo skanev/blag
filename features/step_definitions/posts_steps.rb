a600a436ffddc039dff66d81b6843b3a5e650c1d
Stefan Kanev <stefan.kanev@gmail.com>
2010-05-15 15:38:15 +0200

    Extracted Post field name logic from step
    It's a yuicky way to reuse code, but you've got to
    do what you've got to do.

--- features/step_definitions/posts_steps.rb
+++ features/step_definitions/posts_steps.rb
@@ -1,14 +1,17 @@
+update_post = lambda do |post, field, value|
+  case field
+    when 'Title'        then post.title = value
+    when 'Slug'         then post.slug = value
+    when 'Published at' then post.published_at = Time.parse(value)
+    when 'Old id'       then post.old_id = value
+    else raise "Unknown field: #{field}"
+  end
+end
+
 Given 'that I have the following blog post:' do |table|
   post = Post.new
   table.hashes.each do |row|
-    field, value = row.fetch('Field'), row.fetch('Value')
-    case field
-      when 'Title'        then post.title = value
-      when 'Slug'         then post.slug = value
-      when 'Published at' then post.published_at = Time.parse(value)
-      when 'Old id'       then post.old_id = value
-      else raise "Unknown field: #{field}"
-    end
+    update_post.call post, row.fetch('Field'), row.fetch('Value')
   end
   post.save!
 end
